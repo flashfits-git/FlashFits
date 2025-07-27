8a7d78ff4368317dcda440626081ea80851dabae
Antonyefron <antonyefron007@gmail.com>
2025-07-27 17:23:01 +0530

    git store detail page completed

--- axiosConfig.js
+++ axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://192.168.43.28:3000/api/',
-  baseURL: 'http://192.168.0.100:3000/api/',
+  baseURL: 'http://192.168.29.18:3000/api/',
   timeout: 10000,
 });
 
